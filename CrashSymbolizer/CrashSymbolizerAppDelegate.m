e273c7c6bbf1d12169f95e9738ec388176540d45
Likid <lijiapeng1412@gmail.com>
2015-12-04 09:19:44 +0800

    add file panel

--- CrashSymbolizer/CrashSymbolizerAppDelegate.m
+++ CrashSymbolizer/CrashSymbolizerAppDelegate.m
@@ -14,7 +14,7 @@
 NSString * const CSDefaultAppFilePath = @"~/Desktop";
 NSString * const CSKeyAppFilePath = @"AppFilePath";
 
-@interface CrashSymbolizerAppDelegate ()
+@interface CrashSymbolizerAppDelegate () <NSOpenSavePanelDelegate>
 
 @property (strong, nonatomic) StackAddressProcessor *processor;
 @property (strong, nonatomic) NSArray *ARMs;
@@ -203,5 +203,67 @@ NSString * const CSKeyAppFilePath = @"AppFilePath";
 
     return YES;
 }
+
+- (void)showFilePanel {
+    NSOpenPanel *openPanel = [NSOpenPanel openPanel];
+    openPanel.delegate = self;
+    openPanel.canChooseDirectories = NO;
+    openPanel.canChooseFiles = YES;
+    openPanel.allowedFileTypes = @[@"dSYM", @""];
+    openPanel.treatsFilePackagesAsDirectories = YES;
+    
+    [openPanel beginSheetModalForWindow:self.window completionHandler:^(NSInteger result) {
+        NSLog(@"%ld", (long)result);
+    }];
+}
+
+#pragma mark - NSOpenSavePanelDelegate
+
+/* Optional - enabled URLs.
+ NSOpenPanel: Return YES to allow the 'url' to be enabled in the panel. Delegate implementations should be fast to avoid stalling the UI. Applications linked on Mac OS 10.7 and later should be prepared to handle non-file URL schemes.
+ NSSavePanel: This method is not called; all urls are always disabled.
+ */
+- (BOOL)panel:(id)sender shouldEnableURL:(NSURL *)url {
+    NSLog(@"%s url:%@", __FUNCTION__, url);
+    return YES;
+}
+
+/* Optional - URL validation for saving and opening files.
+ NSSavePanel: The method is called once by the save panel when the user chooses the Save button. The user is intending to save a file at 'url'. Return YES if the 'url' is a valid location to save to. Note that an item at 'url' may not physically exist yet, unless the user decided to overwrite an existing item. Return NO and fill in the 'outError' with a user displayable error message for why the 'url' is not valid. If a recovery option is provided by the error, and recovery succeeded, the panel will attempt to close again.
+ NSOpenPanel: The method is called once for each selected filename (or directory) when the user chooses the Open button. Return YES if the 'url' is acceptable to open. Return NO and fill in the 'outError' with a user displayable message for why the 'url' is not valid for opening. You would use this method over panel:shouldEnableURL: if the processing of the selected item takes a long time. If a recovery option is provided by the error, and recovery succeeded, the panel will attempt to close again.
+ */
+- (BOOL)panel:(id)sender validateURL:(NSURL *)url error:(NSError **)outError {
+    NSLog(@"%s url:%@", __FUNCTION__, url);
+    
+    self.appFilePathTextField.stringValue = url.path;
+    return YES;
+}
+
+/* Optional - Sent when the user has changed the selected directory to the directory located at 'url'. 'url' may be nil, if the current directory can't be represented by an NSURL object (ie: the media sidebar directory, or the "Computer").
+ */
+- (void)panel:(id)sender didChangeToDirectoryURL:(nullable NSURL *)url {
+        NSLog(@"%s url:%@", __FUNCTION__, url);
+}
+
+/* Optional - Filename customization for the NSSavePanel. Allows the delegate to customize the filename entered by the user, before the extension is appended, and before the user is potentially asked to replace a file.
+ */
+- (nullable NSString *)panel:(id)sender userEnteredFilename:(NSString *)filename confirmed:(BOOL)okFlag {
+        NSLog(@"%s filename:%@", __FUNCTION__, filename);
+    return filename;
+}
+
+/* Optional - Sent when the user clicks the disclosure triangle to expand or collapse the file browser while in NSOpenPanel.
+ */
+- (void)panel:(id)sender willExpand:(BOOL)expanding {
+        NSLog(@"%s url:%d", __FUNCTION__, expanding);
+}
+
+/* Optional - Sent when the user has changed the selection.
+ */
+- (void)panelSelectionDidChange:(nullable id)sender {
+        NSLog(@"%s sender:%@", __FUNCTION__, sender);
+}
+
+
 
 @end
